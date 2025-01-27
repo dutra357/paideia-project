33f5d94605c5dc59bb27dcf280e6baa5636e1036
David Alves Dutra <dadutra@hotmail.com>
2025-01-27 17:01:52 -0300

    Projeto base adicionado. Paideia/Educa.

---
+++ src/main/java/com/dutra/paideia/PaideiaApplication.java
@@ -0,0 +1,13 @@
+package com.dutra.paideia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaideiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaideiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dutra/paideia/PaideiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutra.paideia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaideiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
