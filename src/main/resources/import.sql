ab2501b4042b3665a79c1f26c85749bc8fbe760d
David Alves Dutra <dadutra@hotmail.com>
2025-01-29 08:16:41 -0300

    Seeding para Resource.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,3 +15,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Curso de Java e POO
 
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2024-01-25T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-02-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2024-12-10T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-12-10T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_url, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'www.fsa.br/wp-content/uploads/bruxas.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_url, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'www.fsa.br/wp-content/uploads/bruxas.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_url, type, offer_id) VALUES ('Lives', 'Lives exclusivas para membros', 3, 'www.fsa.br/wp-content/uploads/bruxas.jpg', 0, 1);
